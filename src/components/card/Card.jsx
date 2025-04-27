6285e4d6bff995414c2bff1ad77a8fc6e40d9dae
Amit Raj <rajamitnine@gmail.com>
2025-04-27 12:27:59 +0530

    add loading=lazy property to the image

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,7 +2,7 @@ function Card({ image, title, description }) {
   return (
     <>
       <div className="w-[300px] border-2 p-2 border-gray-300">
-        <img className="w-[100%]" src={image} alt={title} />
+        <img className="w-[100%]" src={image} alt={title} loading="lazy" />
         <h1 className="font-semibold my-2">{title}</h1>
         <p>{description}</p>
         <button className="w-[100%] my-3 p-2 bg-green-600 text-white rounded">
